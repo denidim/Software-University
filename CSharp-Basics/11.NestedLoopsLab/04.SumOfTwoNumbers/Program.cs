2bd850cd974b5a63ee323f0a257dbe9c5b072766
denidim <94020783+denidim@users.noreply.github.com>
2022-02-25 06:38:38 +0000

    Add files via upload

---
+++ CSharp-Basics/11.NestedLoopsLab/04.SumOfTwoNumbers/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _04.SumOfTwoNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int staringNumber = int.Parse(Console.ReadLine());
+            int finalNumber = int.Parse(Console.ReadLine());
+            int magicNumber = int.Parse(Console.ReadLine());
+            int combination = 0;
+            bool isfound = false;
+            for (int i = staringNumber; i <= finalNumber ; i++)
+            {
+                for (int j = staringNumber; j <= finalNumber ; j++)
+                {
+                    combination++;
+                    if (i + j == magicNumber)
+                    {
+                        Console.WriteLine($"Combination N:{combination} ({i} + {j} = {magicNumber})");
+                        isfound = true;
+                        break;
+                    }
+                }
+                if (isfound)
+                {
+                    break;
+                }
+            }
+            if (!isfound)
+            {
+                Console.WriteLine($"{combination} combinations - neither equals {magicNumber}");
+            }
+        }
+    }
+}
---
+++ CSharp-Basics/11.NestedLoopsLab/07.CinemaTickets/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace _07.CinemaTickets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int student = 0;
+            int standard = 0;
+            int kid = 0;
+            int ticketsCount = 0;
+            string movies = Console.ReadLine();
+            while (movies != "Finish")
+            {
+                int seats = int.Parse(Console.ReadLine());
+                int currentMovieTickets = 0;
+                for (int i = 0; i < seats; i++)
+                {
+                    string type = Console.ReadLine();
+                    if(type == "End")
+                    {
+                        break;
+                    }
+
+                    if (type == "standard")
+                    {
+                        standard++;
+                    }
+                    else if (type == "kid")
+                    {
+                        kid++; 
+                    }
+                    else if (type == "student")
+                    {
+                        student++;
+                    }
+                    ticketsCount++;
+                    currentMovieTickets++;
+                }
+                double hallPercentage = (currentMovieTickets * 1.00 / seats * 1.00) * 100;
+                Console.WriteLine($"{movies} - {hallPercentage:f2}% full.");
+                movies = Console.ReadLine();
+            }
+
+            double studentP = (student * 1.00 / ticketsCount * 1.00) * 100;
+            double standartP = (standard * 1.00 / ticketsCount * 1.00) * 100;
+            double kidsP = (kid * 1.00 / ticketsCount * 1.00) * 100;
+            Console.WriteLine($"Total tickets: {ticketsCount}");
+            Console.WriteLine($"{studentP:f2}% student tickets.");
+            Console.WriteLine($"{standartP:f2}% standard tickets.");
+            Console.WriteLine($"{kidsP:f2}% kids tickets.");
+        }
+    }
+}
