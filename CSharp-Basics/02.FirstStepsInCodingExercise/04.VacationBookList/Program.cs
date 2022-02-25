3bfda291209f24cf917f32112b8233c793524155
denidim <94020783+denidim@users.noreply.github.com>
2022-02-25 06:21:34 +0000

    Add files via upload

---
+++ CSharp-Basics/02.FirstStepsInCodingExercise/04.VacationBookList/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _04.VacationBookList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pages = int.Parse(Console.ReadLine());
+            var pH = double.Parse(Console.ReadLine());
+            var days = int.Parse(Console.ReadLine());
+            var result = (pages / pH) / days;
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ CSharp-Basics/02.FirstStepsInCodingExercise/05.BirthdayParty/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _05.BirthdayParty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rent = double.Parse(Console.ReadLine());
+            var cake = (20/100.0)*rent;
+            var drinks = cake - ((45 / 100.0) * cake);
+            var clown = 1 * (rent / 3);
+            var budget = rent + cake + drinks + clown;
+            Console.WriteLine(budget);
+        }
+    }
+}
