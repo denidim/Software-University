2378bb683c14cd6cf2c08a97b866000fddbec114
Denislav Dimitrov <94020783+denidim@users.noreply.github.com>
2022-02-26 07:48:56 +0000

    Added tasks from SoftUni Interfaces and Abstraction -  lab

---
+++ CSharp-OOP/car/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace Cars
+{
+    public interface IElectricCar
+    {
+        int Battery { get; }
+    }
+
+    public interface ICar
+    {
+        string Model { get; }
+        string Color { get; }
+        string Start();
+        string Stop();
+    }
+
+    public abstract class Car : ICar
+    {
+        public string Model { get; private set;}
+
+        public string Color { get; private set; }
+
+        public Car(string model,string color)
+        {
+            Model = model;
+            Color = color;
+        }
+
+        public string Start()
+        {
+            return "Engine start";
+        }
+
+        public string Stop()
+        {
+            return "Breaaak!";
+        }
+    }
+
+    public class Tesla : Car, IElectricCar
+    {
+        public int Battery { get; private set; }
+
+        public Tesla(string model, string color, int battery) : base(model, color)
+        {
+            Battery = battery;
+        }
+
+        public override string ToString()
+        {
+            return $"{Color} {this.GetType().Name} {Model} with {Battery}";
+        }
+    }
+
+    public class Seat : Car
+    {
+        public Seat(string model, string color):base(model,color)
+        {
+        }
+
+        public override string ToString()
+        {
+            return $"{Color} {this.GetType().Name} {Model}";
+        }
+    }
+
+
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Car seat = new Seat("Leon", "Grey");
+            Console.WriteLine(seat.ToString());
+            Console.WriteLine(seat.Start());
+            Console.WriteLine(seat.Stop());
+
+            Car tesla = new Tesla("Model 3", "Red", 2);
+            Console.WriteLine(tesla.ToString());
+            Console.WriteLine(tesla.Start());
+            Console.WriteLine(tesla.Stop());
+
+        }
+    }
+}
