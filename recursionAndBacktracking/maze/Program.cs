2c0925f69d91d04486c39e3c35e8b4ca39f2f09c
Denislav Dimitrov <94020783+denidim@users.noreply.github.com>
2022-04-17 01:00:15 +0300

    added eightQueensProblem and maze from recursion and backtracking exercise

---
+++ recursionAndBacktracking/maze/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+
+namespace maze
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int rows = int.Parse(Console.ReadLine());
+            //int cols = int.Parse(Console.ReadLine());
+            //char[,] maze = new char[rows, cols];
+            //for (int i = 0; i < rows; i++)
+            //{
+            //    string input = Console.ReadLine();
+            //    for (int j = 0; j < cols; j++)
+            //    {
+            //        maze[i, j] += input[j];
+            //    }
+            //}
+
+            //char[,] maze = new char[,]
+            //{
+            //    { '-','-','-' ,'-'},
+            //    { '*','-','*' ,'-'},
+            //    { '-','-','*' ,'-'},
+            //    { '-','*','-' ,'-'},
+            //    { '-','-','-' ,'E'},
+            //};
+
+            char[,] maze = new char[,]
+            {
+                { '-','-','-'},
+                { '-','*','-'},
+                { '-','*','E'},
+            };
+
+            int row = 0;
+            int col = 0;
+            bool[,] visited = new bool[maze.GetLength(0), maze.GetLength(1)];
+            string path = "";
+
+            FindPath(maze, row, col, visited, path);
+        }
+
+        private static void FindPath(char[,] maze, int row, int col, bool[,] visited, string path)
+        {
+            if (maze[row,col] == 'E')
+            {
+                Console.WriteLine("Path: " + path);
+                Print(maze);
+                return;
+            }
+
+            visited[row, col] = true;
+
+            if (IsSafe(maze, row + 1, col, visited))
+            {
+                if (maze[row + 1,col] != 'E')
+                    maze[row + 1,col] = 'D';
+
+                FindPath(maze, row + 1, col, visited, path + 'D');
+            }
+            if (IsSafe(maze, row - 1, col, visited))
+            {
+                if (maze[row - 1,col] != 'E')
+                    maze[row - 1,col] = 'U';
+
+                FindPath(maze, row - 1, col, visited, path + 'U');
+            }
+            if (IsSafe(maze, row, col + 1, visited))
+            {
+                if (maze[row,col + 1] != 'E')
+                    maze[row,col + 1] = 'R';
+
+                FindPath(maze, row, col + 1, visited, path + 'R');
+            }
+            if (IsSafe(maze, row, col-1, visited))
+            {
+                if (maze[row,col - 1] != 'E')
+                    maze[row,col - 1] = 'L';
+
+                FindPath(maze, row, col-1, visited, path + 'L');
+            }
+
+            visited[row, col] = false;
+            maze[row,col] = '-';
+
+        }
+        private static bool IsSafe(char[,] maze,int row,int col,bool[,] visited)
+        {
+            if (row < 0 || col < 0 || row >= maze.GetLength(0) || col >= maze.GetLength(1))
+            {
+                return false;
+            }
+            if (maze[row,col] == '*'|| visited[row,col])
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private static void Print(char[,] maze)
+        {
+            for (int row = 0; row < maze.GetLength(0); row++)
+            {
+                for (int col = 0; col < maze.GetLength(1); col++)
+                {
+                    Console.Write(maze[row,col]);
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ recursionAndBacktracking/recursionDemo/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace recursionDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int size = int.Parse(Console.ReadLine());
+
+            //char[][] matrix = new char[size][];
+            //for (int i = 0; i < size; i++)
+            //{
+            //    var currRow = Console.ReadLine();
+            //    matrix[i] = currRow.ToCharArray();
+            //}
+
+            Dictionary<List<int>, string> position = new Dictionary<List<int>, string>();
+            position.Add(new List<int> { 0, 0 }, "D");
+            List<int> arr = new List<int> { 0,0  };
+
+
+            var curr = position.FirstOrDefault(x => x.Key.SequenceEqual(arr));
+
+            position.Remove();
+
+
+            // string[] maze = new string[]
+            //{
+            //     "010001",
+            //     "01010E",
+            //     "010100",
+            //     "000000",
+            //};
+
+            // for (int i = 0; i < maze.Length; i++)
+            // {
+            //     for (int j = 0; j < maze[i].Length; j++)
+            //     {
+
+            //         Console.Write(maze[i][j]);
+            //     }
+            //     Console.WriteLine();
+            // }
+
+            ////Print(10);
+            //int[] array = new int[] { 3, 8, 9};
+            ////int sum = 0;
+            ////for (int i = 0; i < array.Length; i++)
+            ////{
+            ////    sum += array[i];
+            ////}
+            //Console.WriteLine(Sum(array,0));
+        }
+
+        static int Sum(int[] array,int i)
+        {
+            if (i == array.Length)
+            {
+                return 0;
+            }
+
+            Console.WriteLine("Befor entering recursive method");
+
+            int currSum = array[i] + Sum(array, i + 1);
+
+            Console.WriteLine(currSum);
+
+            Console.WriteLine("After entering recursive method");
+
+            return currSum;
+        }
+
+        static void Print(int n)
+        {
+            if (n == 0)
+            {
+                return;
+            }
+            Console.WriteLine(" Somthing");
+            Console.WriteLine(n);
+            Print(n - 1);
+        }
+    }
+}
