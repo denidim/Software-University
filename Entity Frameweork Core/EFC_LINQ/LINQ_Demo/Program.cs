a820504899df3b550c1770e4a5d9829a4a6a76fa
denidim <fs900220@gmail.com>
2022-07-17 20:19:48 +0300

    changes

---
+++ Entity Frameweork Core/EFC_LINQ/LINQ_Demo/Program.cs
@@ -0,0 +1,21 @@
+﻿using LINQ_Demo.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+
+namespace LINQ_Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var db = new MusicXContext();
+
+            db.Songs
+                .Where(x => x.Source.Name == "User")
+                .Select(x => new {x.Name,Source = x.Source.Name})
+                .ToList()
+                .ForEach(x => Console.WriteLine(x.Name));
+        }
+    }
+}
