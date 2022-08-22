7337ec5b1d10d52b87afc90b8f002d47fd0aca51
denidim <fs900220@gmail.com>
2022-08-22 12:58:11 +0300

    vs git test chenges

---
+++ CSharp-Web-Basics/BasicWebServer/TEST/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TEST
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
