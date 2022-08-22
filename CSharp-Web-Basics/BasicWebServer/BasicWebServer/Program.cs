498e69bb6531f04c4432d71f1fefdbb36fd180b7
denidim <fs900220@gmail.com>
2022-08-22 11:42:29 +0300

    deleted custom StringExtension -not needed

---
+++ CSharp-Web-Basics/BasicWebServer/BasicWebServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BasicWebServer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
