4b4337bab58a711071a053fe8b51b83796562621
denidim <fs900220@gmail.com>
2022-08-24 15:20:40 +0300

    added Chronometer

---
+++ CSharp-Web-Basics/AsyncDemo/Chronometer/Chronometer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chronometer
+{
+    public class Chronometer : IChronometer
+    {
+        private Stopwatch stopwatch;
+        
+        private List<string> laps { get; }
+
+        public Chronometer()
+        {
+            this.stopwatch = new Stopwatch();
+            this.laps = new List<string>();
+        }
+
+        public string GetTime => this.stopwatch.Elapsed.ToString(@"mm\:ss\.ffff");
+
+        public List<string> Laps => this.laps;
+
+        public void Start()
+        {
+            this.stopwatch.Start();
+        }
+
+        public void Stop()
+        {
+            this.stopwatch.Start();
+        }
+
+        public string Lap()
+        {
+            string result = GetTime;
+            this.laps.Add(result);
+            return result;
+        }
+
+        public void Reset()
+        {
+            this.stopwatch.Reset();
+            this.laps.Clear();
+        }
+    }
+}
---
+++ CSharp-Web-Basics/AsyncDemo/Chronometer/IChronometer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chronometer
+{
+    public interface IChronometer
+    {
+        string GetTime { get; }
+
+        List<string> Laps { get; }
+
+        void Start();
+
+        void Stop();
+
+        string Lap();
+
+        void Reset();
+    }
+}
