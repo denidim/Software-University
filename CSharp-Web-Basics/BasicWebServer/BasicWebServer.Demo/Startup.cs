824863e0d9d219272f5ae51e7cf602ed90fa5a1b
denidim <fs900220@gmail.com>
2022-08-26 00:33:02 +0300

    added cookies

--- CSharp-Web-Basics/BasicWebServer/BasicWebServer.Demo/Startup.cs
+++ CSharp-Web-Basics/BasicWebServer/BasicWebServer.Demo/Startup.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
 using System.IO;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 using BasicWebServer.Server;
 using BasicWebServer.Server.HTTP;
 using BasicWebServer.Server.Responses;
@@ -34,9 +37,45 @@ namespace BasicWebServer.Demo
                 .MapGet("/HTML", new HtmlResponse(Startup.HtmlForm))
                 .MapPost("/HTML", new TextResponse("", Startup.AddFormDataAction))
                 .MapGet("/Content", new HtmlResponse(Startup.DownloadForm))
-                .MapPost("/Content", new TextFileResponse(Startup.FileName)));
+                .MapPost("/Content", new TextFileResponse(Startup.FileName))
+                .MapGet("/Cookies", new HtmlResponse("", Startup.AddCookiesAction)));
 
             await server.Start();
+        }
+
+        private static void AddCookiesAction(Request request, Response response)
+        {
+            var requestHasCookies = request.Cookies.Any();
+            var bodyText = "";
+
+            if (requestHasCookies)
+            {
+                var cookieText = new StringBuilder();
+
+                cookieText.AppendLine("<h1>Cookies</h1>");
+
+                cookieText.Append("<table border='1'><tr><th>Name</th><th>Value</th></tr>");
+
+                foreach (var cookie in request.Cookies)
+                {
+                    cookieText.Append("<tr>");
+                    cookieText.Append($"<td>{HttpUtility.HtmlEncode(cookie.Name)}</td>");
+                    cookieText.Append($"<td>{HttpUtility.HtmlEncode(cookie.Value)}</td>");
+                    cookieText.Append("</tr>");
+                }
+                cookieText.Append("</table");
+
+                bodyText = cookieText.ToString();
+            }
+            else
+            {
+                bodyText = "<h1>Cookies set!</h1>";
+                response.Cookies.Add("My-Cookie", "My-Value");
+                response.Cookies.Add("My-Second-Cookie", "My-Second-Value");
+            }
+
+
+            response.Body = bodyText;
         }
             
 
---
+++ CSharp-Web-Basics/BasicWebServer/BasicWebServer.Server/HTTP/CookieCollection.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicWebServer.Server.HTTP
+{
+    public class CookieCollection : IEnumerable<Cookie>
+    {
+        private readonly Dictionary<string, Cookie> cookies;
+
+        public CookieCollection() => this.cookies = new Dictionary<string, Cookie>();
+
+        public string this[string name] => this.cookies[name].Value;
+
+        public void Add(string name, string value) => this.cookies[name] = new Cookie(name, value);
+
+        public bool Contains(string name) => this.cookies.ContainsKey(name);
+
+        public IEnumerator<Cookie> GetEnumerator() => this.cookies.Values.GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+    }
+}
