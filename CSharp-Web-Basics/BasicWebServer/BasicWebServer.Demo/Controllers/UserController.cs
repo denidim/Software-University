17fb6433d26ff22324f96d30d9dcbc94a663c3a6
denidim <fs900220@gmail.com>
2022-09-27 16:02:11 +0300

    added User Contreller

---
+++ CSharp-Web-Basics/BasicWebServer/BasicWebServer.Demo/Controllers/UserController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BasicWebServer.Server.Controller;
+using BasicWebServer.Server.HTTP;
+
+namespace BasicWebServer.Demo.Controllers
+{
+    public class UserController : Controller
+    {
+        private const string LoginForm = @"<form action='/Login' method='POST'>
+            Username: <input type='text' name='Username'/>
+            Password: <input type='text' name='Password'/>
+            <input type='submit' value ='Log In' />
+        </form>";
+
+        public UserController(Request request) : base(request)
+        {
+        }
+
+        public Response Login() => Html(UserController.LoginForm);
+    }
+}
