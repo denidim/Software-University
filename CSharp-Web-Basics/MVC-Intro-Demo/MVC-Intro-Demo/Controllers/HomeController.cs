f9af13df6270b11fa06792e711d17db94701939a
denidim <fs900220@gmail.com>
2022-10-06 20:53:32 +0300

    added mvc-intor-demo

---
+++ CSharp-Web-Basics/MVC-Intro-Demo/MVC-Intro-Demo/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_Intro_Demo.Models;
+using System.Diagnostics;
+
+namespace MVC_Intro_Demo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.Message = "Hello World!";
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "This is an ASP.NET Core MVC app.";
+            return View();
+        }
+
+        public ActionResult Numbers()
+        {
+            return View();
+        }
+
+        public ActionResult NumbersToN(int count = 3)
+        {
+            ViewBag.Count = count;
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ CSharp-Web-Basics/MVC-Intro-Demo/MVC-Intro-Demo/Controllers/ProductsController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_Intro_Demo.Models;
+
+namespace MVC_Intro_Demo.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private IEnumerable<ProductViewModel> products
+            = new List<ProductViewModel>()
+            {
+                new ProductViewModel()
+                {
+                    Id = 1,
+                    Name = "Cheese",
+                    Price = 7.00,
+                },
+                new ProductViewModel()
+                {
+                    Id = 2,
+                    Name = "Ham",
+                    Price = 5.50,
+                },
+                new ProductViewModel()
+                {
+                    Id = 3,
+                    Name = "Bread",
+                    Price = 1.50,
+                }
+            };
+
+        public ActionResult All()
+        {
+            return View(this.products);
+        }
+
+        public IActionResult ById(int id)
+        {
+            var product = this.products.FirstOrDefault(p => p.Id == id);
+
+            if (product == null)
+            {
+                return BadRequest();
+            }
+
+            return View(product);
+        }
+    }
+}
