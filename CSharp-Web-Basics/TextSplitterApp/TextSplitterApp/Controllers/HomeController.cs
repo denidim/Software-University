bec2ca79ba637c78e035bece87b29f16da637754
denidim <fs900220@gmail.com>
2022-10-07 16:03:44 +0300

    added logic

--- CSharp-Web-Basics/TextSplitterApp/TextSplitterApp/Controllers/HomeController.cs
+++ CSharp-Web-Basics/TextSplitterApp/TextSplitterApp/Controllers/HomeController.cs
@@ -13,9 +13,19 @@ namespace TextSplitterApp.Controllers
             _logger = logger;
         }
 
-        public IActionResult Index()
+        public IActionResult Index(TextViewModel model)
         {
-            return View();
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Split(TextViewModel model)
+        {
+            var splittextArray = model.Text.Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+            model.SplitText = string.Join(Environment.NewLine, splittextArray);
+
+            return RedirectToAction("Index", model);
         }
 
         public IActionResult Privacy()
---
+++ CSharp-Web-Basics/TextSplitterApp/TextSplitterApp/Models/TextViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace TextSplitterApp.Models
+{
+    public class TextViewModel
+    {
+        public string Text { get; set; }
+
+        public string SplitText { get; set; }
+    }
+}
