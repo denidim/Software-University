60a9f13dcdfe6601d9cb305506c6473cacd427b3
denidim <fs900220@gmail.com>
2022-10-31 17:45:54 +0200

    sorted usings

--- CSharp-Web-Advanced/MyRecipes/Data/MyRecipes.Data/ApplicationDbContext.cs
+++ CSharp-Web-Advanced/MyRecipes/Data/MyRecipes.Data/ApplicationDbContext.cs
@@ -6,11 +6,11 @@
     using System.Threading;
     using System.Threading.Tasks;
 
-    using MyRecipes.Data.Common.Models;
-    using MyRecipes.Data.Models;
-
     using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
     using Microsoft.EntityFrameworkCore;
+
+    using MyRecipes.Data.Common.Models;
+    using MyRecipes.Data.Models;
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, string>
     {
