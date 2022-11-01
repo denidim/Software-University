a7efc2454f020aa88858aeb3748f7ae9c9e3ff12
denidim <fs900220@gmail.com>
2022-11-01 07:15:48 +0200

    sorted usings

--- CSharp-Web-Advanced/MyRecipes/Web/MyRecipes.Web/Program.cs
+++ CSharp-Web-Advanced/MyRecipes/Web/MyRecipes.Web/Program.cs
@@ -2,6 +2,13 @@
 {
     using System.Reflection;
 
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Hosting;
     using MyRecipes.Data;
     using MyRecipes.Data.Common;
     using MyRecipes.Data.Common.Repositories;
@@ -12,14 +19,6 @@
     using MyRecipes.Services.Mapping;
     using MyRecipes.Services.Messaging;
     using MyRecipes.Web.ViewModels;
-
-    using Microsoft.AspNetCore.Builder;
-    using Microsoft.AspNetCore.Http;
-    using Microsoft.AspNetCore.Mvc;
-    using Microsoft.EntityFrameworkCore;
-    using Microsoft.Extensions.Configuration;
-    using Microsoft.Extensions.DependencyInjection;
-    using Microsoft.Extensions.Hosting;
 
     public class Program
     {
