1af06d6f67252f86869f930def85bcd68633e63e
denidim <fs900220@gmail.com>
2022-11-03 11:03:17 +0200

    added ProductApiController

---
+++ CSharp-Web-Basics/WebShopDemo/WebShopDemo/Controllers/ProductApiController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebShopDemo.Controllers
+{
+    public class ProductApiController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
