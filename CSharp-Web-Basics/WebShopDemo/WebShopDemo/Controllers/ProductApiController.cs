500ab6e9ec395a7845b98e5ea48bc336b7c36c84
denidim <fs900220@gmail.com>
2022-11-03 11:41:07 +0200

    added api controller

--- CSharp-Web-Basics/WebShopDemo/WebShopDemo/Controllers/ProductApiController.cs
+++ CSharp-Web-Basics/WebShopDemo/WebShopDemo/Controllers/ProductApiController.cs
@@ -1,12 +1,35 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebShopDemo.Core.Data.Models;
 
 namespace WebShopDemo.Controllers
 {
+    [Route("api/[controller]")]
+    [ApiController]
     public class ProductApiController : Controller
     {
-        public IActionResult Index()
+        [HttpGet]
+        public Product Test()
         {
-            return View();
+            return new Product()
+            {
+                Name = "Some Test Product",
+                Description = "Some description",
+                Price = 3.2M,
+                Quantity = 20,
+                IsActive = true,    
+            };
+        }
+
+        [HttpDelete]
+        public string SoftUni()
+        {
+            return "Delete";
+        }
+
+        [HttpPost]
+        public string SoftUni1()
+        {
+            return "Post";
         }
     }
 }
