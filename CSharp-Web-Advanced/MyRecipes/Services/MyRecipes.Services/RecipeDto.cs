e6ca19bb81c942e2960482b163115aed9cc93782
denidim <fs900220@gmail.com>
2022-11-05 14:36:38 +0200

    changes

--- CSharp-Web-Advanced/MyRecipes/Services/MyRecipes.Services/RecipeDto.cs
+++ CSharp-Web-Advanced/MyRecipes/Services/MyRecipes.Services/RecipeDto.cs
@@ -1,12 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MyRecipes.Services
+﻿namespace MyRecipes.Services
 {
-    internal class RecipeDto
+    using System;
+    using System.Collections.Generic;
+
+    public class RecipeDto
     {
+        public string Name { get; set; }
+
+        public string Instructions { get; set; }
+
+        public TimeSpan? PreparationTime { get; set; }
+
+        public TimeSpan? CookingTime { get; set; }
+
+        public int? PortionCount { get; set; }
+
+        public string OriginalUrl { get; set; }
+
+        public string CreatedByUserId { get; set; }
+
+        public string CategoryName { get; set; }
+
+        public string Image { get; set; }
+
+        public string ImageExtension { get; set; }
+
+        public ICollection<Dictionary<string, string>> Ingridients { get; set; }
+            = new List<Dictionary<string, string>>();
     }
 }
