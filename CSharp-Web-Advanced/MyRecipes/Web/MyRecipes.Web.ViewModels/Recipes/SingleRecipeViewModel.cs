f1e227d84c6d8b4357dce3a60150ad795982f236
denidim <fs900220@gmail.com>
2022-11-08 19:15:10 +0200

    added vote to dto

---
+++ CSharp-Web-Advanced/MyRecipes/Web/MyRecipes.Web.ViewModels/Recipes/SingleRecipeViewModel.cs
@@ -0,0 +1,54 @@
+﻿namespace MyRecipes.Web.ViewModels.Recipes
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using AutoMapper;
+    using MyRecipes.Data.Models;
+    using MyRecipes.Services.Mapping;
+
+    public class SingleRecipeViewModel : IMapFrom<Recipe>, IHaveCustomMappings
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string CategoryName { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public string AddedByUserEmail { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public string Instructions { get; set; }
+
+        public TimeSpan PreparationTime { get; set; }
+
+        public TimeSpan CookingTime { get; set; }
+
+        public int PortionCount { get; set; }
+
+        public int CategoryRecipesCount { get; set; }
+
+        public string OriginalUrl { get; set; }
+
+        public double VotesAverageValue { get; set; }
+
+        public IEnumerable<IngredientsViewModel> Ingredients { get; set; }
+
+        public void CreateMappings(IProfileExpression configuration)
+        {
+            configuration.CreateMap<Recipe, SingleRecipeViewModel>()
+                .ForMember(x => x.VotesAverageValue, options =>
+                options.MapFrom(x =>
+                x.Votes.Average(x => x.Value)))
+                .ForMember(x => x.ImageUrl, options =>
+                options.MapFrom(x =>
+                x.Images.FirstOrDefault().ImageUrl != null ?
+                    x.Images.FirstOrDefault().ImageUrl :
+                    "/images/recipes/" + x.Images.FirstOrDefault().Id + "." + x.Images.FirstOrDefault().Extension));
+        }
+    }
+}
