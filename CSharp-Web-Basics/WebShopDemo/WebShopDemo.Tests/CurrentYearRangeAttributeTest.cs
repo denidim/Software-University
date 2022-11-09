84f091c031a5a4d6240db485d0c40924c574f303
denidim <fs900220@gmail.com>
2022-11-09 12:35:10 +0200

    added test examples

---
+++ CSharp-Web-Basics/WebShopDemo/WebShopDemo.Tests/CurrentYearRangeAttributeTest.cs
@@ -0,0 +1,53 @@
+﻿using WebShopDemo.Core.ValidationAttributes;
+
+namespace WebShopDemo.Tests
+{
+    
+    public class CurrentYearRangeAttributeTest
+    {
+        [Fact]
+        public void IsValidReturnsFalseForDateTimeAfterCurrentYear()
+        {
+            //Arange
+            var attribute = new CurrentYearRange(199);
+
+            //Act
+            var isValid = attribute.IsValid(DateTime.UtcNow.AddYears(1));
+
+            //Assert
+
+            Assert.False(isValid);
+        }
+
+        [Fact]
+        public void IsValidReturnsFalseForYearAfterCurrentYear()
+        {
+            //Arange
+            var attribute = new CurrentYearRange(199);
+
+            //Act
+            var isValid = attribute.IsValid(DateTime.UtcNow.AddYears(1).Year);
+
+            //Assert
+
+            Assert.False(isValid);
+        }
+
+        [Fact]
+        public void IsValidReturnsTrueForYearBeforeCurrentYear()
+        {
+            //Arange
+            var attribute = new CurrentYearRange(199);
+
+            //Act
+            var isValid = attribute.IsValid(DateTime.UtcNow.AddYears(-1).Year);
+
+            //Assert
+
+            Assert.True(isValid);
+        }
+
+
+
+    }
+}
---
+++ CSharp-Web-Basics/WebShopDemo/WebShopDemo.Tests/UnitTest1.cs
@@ -0,0 +1,20 @@
+namespace WebShopDemo.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            // Arrange
+            var a = 2;
+            var b = 2;
+
+            // Act
+            Assert.Throws<DivideByZeroException>(() => a + b / 0);
+            var sum = a + b;
+
+            // Assert
+            Assert.Equal(4, sum);
+        }
+    }
+}
