8b658945d6ecf4f7ce4a3c02c08e757e815f3e30
denidim <fs900220@gmail.com>
2022-11-12 14:22:00 +0200

    added area with scaffold administration for Categories

---
+++ CSharp-Web-Advanced/MyRecipes/Web/MyRecipes.Web/Areas/Administration/Controllers/CategoriesController.cs
@@ -0,0 +1,168 @@
+﻿namespace MyRecipes.Web.Areas.Administration.Controllers
+{
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.EntityFrameworkCore;
+    using MyRecipes.Data;
+    using MyRecipes.Data.Common.Repositories;
+    using MyRecipes.Data.Models;
+
+    [Area("Administration")]
+    public class CategoriesController : AdministrationController
+    {
+        private readonly IDeletableEntityRepository<Category> categoryRepo;
+
+        public CategoriesController(IDeletableEntityRepository<Category> categoryRepo)
+        {
+            this.categoryRepo = categoryRepo;
+        }
+
+        // GET: Administration/Categories
+        public async Task<IActionResult> Index()
+        {
+              return this.View(await this.categoryRepo.AllWithDeleted().ToListAsync());
+        }
+
+        // GET: Administration/Categories/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || this.categoryRepo.All() == null)
+            {
+                return this.NotFound();
+            }
+
+            var category = await this.categoryRepo.All()
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (category == null)
+            {
+                return this.NotFound();
+            }
+
+            return this.View(category);
+        }
+
+        // GET: Administration/Categories/Create
+        public IActionResult Create()
+        {
+            return this.View();
+        }
+
+        // POST: Administration/Categories/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Name,IsDeleted,DeletedOn,Id,CreatedOn,ModifiedOn")] Category category)
+        {
+            if (this.ModelState.IsValid)
+            {
+                await this.categoryRepo.AddAsync(category);
+                await this.categoryRepo.SaveChangesAsync();
+                return this.RedirectToAction(nameof(this.Index));
+            }
+
+            return this.View(category);
+        }
+
+        // GET: Administration/Categories/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return this.NotFound();
+            }
+
+            var category = this.categoryRepo.All().FirstOrDefault(x => x.Id == id);
+
+            if (category == null)
+            {
+                return this.NotFound();
+            }
+
+            return this.View(category);
+        }
+
+        // POST: Administration/Categories/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Name,IsDeleted,DeletedOn,Id,CreatedOn,ModifiedOn")] Category category)
+        {
+            if (id != category.Id)
+            {
+                return this.NotFound();
+            }
+
+            if (this.ModelState.IsValid)
+            {
+                try
+                {
+                    this.categoryRepo.Update(category);
+                    await this.categoryRepo.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!this.CategoryExists(category.Id))
+                    {
+                        return this.NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+
+                return this.RedirectToAction(nameof(this.Index));
+            }
+
+            return this.View(category);
+        }
+
+        // GET: Administration/Categories/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || this.categoryRepo.All() == null)
+            {
+                return this.NotFound();
+            }
+
+            var category = await this.categoryRepo.All()
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (category == null)
+            {
+                return this.NotFound();
+            }
+
+            return this.View(category);
+        }
+
+        // POST: Administration/Categories/Delete/5
+        [HttpPost]
+        [ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (this.categoryRepo.All() == null)
+            {
+                return this.Problem("Entity set 'ApplicationDbContext.Categories'  is null.");
+            }
+
+            var category = this.categoryRepo.All().FirstOrDefault(x => x.Id == id);
+            if (category != null)
+            {
+                this.categoryRepo.Delete(category);
+            }
+
+            await this.categoryRepo.SaveChangesAsync();
+            return this.RedirectToAction(nameof(this.Index));
+        }
+
+        private bool CategoryExists(int id)
+        {
+          return this.categoryRepo.All().Any(e => e.Id == id);
+        }
+    }
+}
