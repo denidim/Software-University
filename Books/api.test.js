40451ce03e006ccbe302cc649f5c6a1addd1733f
denidim <fs900220@gmail.com>
2024-06-18 14:06:17 +0100

    added tests for GET single book by id

--- Books/api.test.js
+++ Books/api.test.js
@@ -7,13 +7,13 @@ const expect = chai.expect;
 chai.use(chaiHttp);
 
 describe('Books API', () => {
-    it('should POST a book' , (done) => {
-        const book = {id:'1',title:'Test Book',author: 'Test Author'};
+    it('should POST a book', (done) => {
+        const book = { id: '1', title: 'Test Book', author: 'Test Author' };
         chai.request(server)
             .post('/books')
             .send(book)
-            .end((err,res) => {
-                if(err){
+            .end((err, res) => {
+                if (err) {
                     return done(err)
                 }
                 expect(res.statusCode, "Status code").to.be.equal(201);
@@ -22,23 +22,42 @@ describe('Books API', () => {
                 expect(res.body.id, "BookId property").to.be.equal(book.id);
                 expect(res.body.title, "BookTitle property").to.be.equal(book.title);
                 expect(res.body.author, "BookAuthor property").to.be.equal(book.author);
-                console.log("response: ",res.body);
+                console.log("response: ", res.body);
                 done();
             });
     })
 
-    it('should GET all books' , (done) => {
+    it('should GET all books', (done) => {
         chai.request(server)
             .get('/books')
-            .end((err,res) => {
-                if(err){
+            .end((err, res) => {
+                if (err) {
                     return done(err)
                 }
                 expect(res.statusCode, "Status code").to.be.equal(200);
                 expect(res.body[0].id, "BookId property").to.be.equal('1');
                 expect(res.body).to.be.a('array');
-                console.log("response: ",res.body);
+                console.log("response: ", res.body);
                 done();
             });
     })
+
+    it('should GET a single book', (done) => {
+        const bookId = 1;
+        chai.request(server)
+            .get(`/books/${bookId}`)
+            .end((err, res) => {
+                if (err) {
+                    return done(err)
+                }
+                expect(res).to.have.status(200);
+                expect(res.body).to.be.a('object');
+                expect(res.body).to.have.property('id');
+                expect(res.body).to.have.property('title');
+                expect(res.body).to.have.property('author');
+                console.log("response: ", res.body);
+                done();
+            });
+    })
+    
 })
