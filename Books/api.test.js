743b26e7d0c9b5f1cd75fe78288ac8bfd5118107
denidim <fs900220@gmail.com>
2024-06-18 13:16:32 +0100

    added test

--- Books/api.test.js
+++ Books/api.test.js
@@ -1,7 +1,23 @@
 //setting up the dependencies
 const chai = require('chai');
-const chaiHttp = require('chai=http');
+const chaiHttp = require('chai-http');
 const server = require('./server');
 const expect = chai.expect;
 
 chai.use(chaiHttp);
+
+describe('books', () => {
+    it('should be able to create new book' , (done) => {
+        const book = {id:'1',title:'Test Book',author: 'Test Author'};
+        chai.request(server)
+        .post('/books')
+        .send(book)
+        .end((err,res) => {
+            if(err){
+                return done(err)
+            }
+            console.log("response: ",res.body)
+            done();
+        });
+    })
+})
